792a31760db29d2b5c60c7ea13eb948d108b709c
epictete <john.laterre@gmail.com>
2020-10-14 10:43:01 +0200

    Updates

---
+++ precache-manifest.c38a130588d77445ab9daa4ebe88ce74.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8984ecf4d5ab376476898481c33c9ff3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9d6811c2eab489460844",
+    "url": "/static/css/main.d820c58d.chunk.css"
+  },
+  {
+    "revision": "a3886eb48eeb822dea54",
+    "url": "/static/js/2.abb637fc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.abb637fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d6811c2eab489460844",
+    "url": "/static/js/main.b985736e.chunk.js"
+  },
+  {
+    "revision": "fd1e764c12408bbbbfc1",
+    "url": "/static/js/runtime-main.f859b388.js"
+  }
+]);
